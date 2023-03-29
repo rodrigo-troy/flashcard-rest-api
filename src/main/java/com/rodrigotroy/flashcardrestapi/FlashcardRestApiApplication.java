00f532805db043d41c857cecd2fd481e24bcfaf5
rodrigotroy <1328268+rodrigo-troy@users.noreply.github.com>
2023-03-29 18:09:39 -0300

    Add base code for project

---
+++ src/main/java/com/rodrigotroy/flashcardrestapi/FlashcardRestApiApplication.java
@@ -0,0 +1,14 @@
+package com.rodrigotroy.flashcardrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlashcardRestApiApplication.class,
+                              args);
+    }
+
+}
---
+++ src/test/java/com/rodrigotroy/flashcardrestapi/FlashcardRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigotroy.flashcardrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
